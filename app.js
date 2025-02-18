db78c221ecb3e41c5248d9712dfbeb5e3cc3e24d
dmarlop <davidmarinlpz@gmail.com>
2025-02-18 12:59:03 +0100

    funciona?

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('¡Hola Mundo desde Docker y Jenkins prueba de integracion continua!\n');
+  res.end('¡Hola Mundo desde Docker y Jenkins prueba de integracion continua 2.0!\n');
 });
 
 server.listen(port, hostname, () => {
