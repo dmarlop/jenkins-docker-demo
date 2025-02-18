11ad16455dda4911647b22c653b1ed1ed595fd71
dmarlop <davidmarinlpz@gmail.com>
2025-02-18 12:52:30 +0100

    intento de integracion

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('¡Hola Mundo desde Docker y Jenkins!\n');
+  res.end('¡Hola Mundo desde Docker y Jenkins prueba de integracion continua!\n');
 });
 
 server.listen(port, hostname, () => {
