17724998d2322a271691ef5dc33bed22ef092785
dmarlop <davidmarinlpz@gmail.com>
2025-02-18 12:31:14 +0100

    prueba de fuego

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('¡Hola Mundo desde Docker y Jenkins sdsdf!\n');
+  res.end('¡Hola Mundo desde Docker y Jenkins!\n');
 });
 
 server.listen(port, hostname, () => {
